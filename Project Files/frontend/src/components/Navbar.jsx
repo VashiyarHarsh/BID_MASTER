c8e22c842761f2feb254549e8080417ef0804404
sania053 <202201053@daiict.ac.in>
2024-11-16 15:18:14 +0530

    Navbar and Footer frontend final change

--- Project Files/frontend/src/components/Navbar.jsx
+++ Project Files/frontend/src/components/Navbar.jsx
@@ -1,28 +1,29 @@
 import { NavLink } from "react-router-dom";
 import "./Navbar.css";
+
 export const Navbar = () => {
   return (
-    <>
-      <header>
-        <div className="container">
-          <div className="logo-brand">
-            {/* Correct the component name to NavLink */}
-            <NavLink to="/"><p>BIDMASTER</p></NavLink>
-          </div>
+    <header className="navbar">
+      <div className="container">
+        <div className="logo-brand">
+          <NavLink to="/" className="logo-link">
+          
+  <span className="bidmaster">B</span>
+  <span className="bidmaster id-highlight">iD</span>
+  <span className="bidmaster">Master</span>
 
-          <nav>
-            <ul>
-              <li><NavLink to="/">HOME</NavLink></li>
-              <li><NavLink to="/ViewProducts">PRODUCTS</NavLink></li>
 
-              <li><NavLink to="/BidLive">BID LIVE</NavLink></li>
-              <li><NavLink to="/AddProducts">ADD PRODUCT</NavLink></li>
-              <li><NavLink to="/Login">LOGIN</NavLink></li>
-              <li><NavLink to="/profile">PROFILE</NavLink></li>
-            </ul>
-          </nav>
+          </NavLink>
         </div>
-      </header>
-    </>
+        <nav>
+          <ul className="nav-links">
+            <li><NavLink to="/" activeClassName="active">Home</NavLink></li>
+            <li><NavLink to="/ViewProducts" activeClassName="active">Products</NavLink></li>
+            <li><NavLink to="/AddProducts" activeClassName="active">Add Product</NavLink></li>
+            <li><NavLink to="/Login" activeClassName="active">Sign In</NavLink></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
   );
 };
