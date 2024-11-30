f309a9d586eae1b15d0f572e82afef17750e1062
HarshV206 <128570425+HarshV206@users.noreply.github.com>
2024-11-30 19:41:45 +0530

    fixed A a error

--- Project Files/frontend/src/pages/AddProducts.jsx
+++ Project Files/frontend/src/pages/AddProducts.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./addProducts.css";
+import "./AddProducts.css";
 
 const AddProducts = () => {
   const navigate = useNavigate();
