c57007f772bf20c83d72fb10a3512811a81c1c2d
HarshV206 <128570425+HarshV206@users.noreply.github.com>
2024-12-01 13:31:06 +0530

    Payload limit testing

--- Project_Files/backend/server.js
+++ Project_Files/backend/server.js
@@ -25,15 +25,15 @@ app.use(cookieParser());
 app.use(cors(corsOptions));
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.urlencoded({extended:false}));
-app.use(express.json());
+app.use(express.urlencoded({ extended:false, limit: '50mb' }));
+app.use(express.json({  limit: '50mb' }));
 //app.use("/", router);
 app.use("/api/form", productRoute);
 app.use("/filter", categoryRouter);
 app.use("/user", userRouter);
 app.use("/mail", mailRouter);
 
-app.use(fileUpload());
+app.use(fileUpload({ limits: { fileSize: 50 * 1024 * 1024 } }));
 app.get("/", (req, res) => {
     res.send("Hello from the server!");
   });
